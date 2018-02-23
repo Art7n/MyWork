cda4c075124777aafaaa2bbcb463f1b4cdd25ca1
Art7n <Teimun777@gmail.com>
2018-02-23 19:48:18 +0200

    bla

---
+++ src/classWork/lesson8/Student.java
@@ -0,0 +1,41 @@
+package classWork.lesson8;
+
+/**
+ * Created by Student on 23.02.2018.
+ */
+public class Student {
+
+    private String firstName;
+    private String lastName;
+    private String group;
+
+    public Student(String group, String lastName, String firstName) {
+        this.group = group;
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+}
